6b5859e939265ce0ecce961ce765ee19be3afa33
burhan orkun <beyazmartilar@gmail.com>
2023-07-11 16:36:42 +0300

    new api version - v2

---
+++ src/test/java/com/orkun/DepartmentApiApplication.java
@@ -0,0 +1,22 @@
+package com.orkun;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@EnableAsync
+@OpenAPIDefinition(info =
+@Info(title = "Department API",
+        version = "${springdoc.version}",
+        description = "Documentation ${spring.application.name} v1.0")
+)
+@SpringBootApplication
+public class DepartmentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DepartmentApiApplication.class, args);
+    }
+
+}
